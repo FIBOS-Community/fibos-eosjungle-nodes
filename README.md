79ecc4fc13fd4fa876dcdaf48379bac72fbba9f5
cao <huntformm@gmail.com>
2018-11-27 18:12:34 +0800

    fibos for eos

---
+++ README.md
@@ -0,0 +1,47 @@
+# FIBOS-EOS-NODES
+
+	快速启动 EOS 节点
+
+	默认数据，配置存储位置 ./blockData/data
+
+
+
+## 启动说明
+
+说明:
+	
+	fibos bp.js   //快去启动一个bp节点
+
+	fibos seed.js // 快速启动一个同步节点
+
+## 工具
+	
+	FIBOS快速安装
+
+		curl -s https://fibos.io/download/installer.sh |sh	
+
+
+
+	netstat -aln|grep  '内网ip:9870' | grep -c 'ESTABLISHED'    //find how many people connet you
+
+## fibos
+	
+快速启动fibos节点: [https://github.com/FIBOS-Community/fibos-nodes](https://github.com/FIBOS-Community/fibos-nodes)
+	
+官网：[https://fibos.io](https://fibos.io) 
+	
+开发者社区：[https://dev.fo](https://dev.fo)
+
+
+## Jungle2.0
+
+[https://github.com/EOS-Jungle-Testnet/Node-Manual-Installation](https://github.com/EOS-Jungle-Testnet/Node-Manual-Installation)
+
+## CryptoKylin
+
+[https://github.com/eosstore/cryptokylin-illustrate](https://github.com/eosstore/cryptokylin-illustrate)
+
+
+
+
+
---
+++ seed.js
@@ -0,0 +1,46 @@
+var fibos = require('fibos');
+var fs = require("fs");
+var config = require('./config');
+var p2p_peer_address = require('./p2p');
+console.notice("start FIBOS for EOS seed node");
+
+
+fibos.config_dir = config.config_dir;
+fibos.data_dir = config.data_dir;
+
+console.notice("config_dir:", fibos.config_dir);
+console.notice("data_dir:", fibos.data_dir);
+
+
+fibos.load("http", {
+	"http-server-address": "0.0.0.0:8870",
+	"access-control-allow-origin": "*",
+	"http-validate-host": false,
+	"verbose-http-errors": true //打开报错
+});
+
+
+fibos.load("net", {
+	"p2p-peer-address": p2p_peer_address,
+	"max-clients": 100,
+	"p2p-listen-endpoint": "0.0.0.0:9870"
+});
+
+var chain_config = {
+	"contracts-console": true,
+	'chain-state-db-size-mb': config["chain-state-db-size-mb"]
+};
+
+if (!fs.exists(fibos.data_dir) && !fs.exists(fibos.config_dir)) {
+	chain_config['genesis-json'] = "genesis.json";
+}
+
+fibos.load("producer");
+
+fibos.load("chain", chain_config);
+fibos.load("chain_api");
+
+fibos.pubkey_prefix = "EOS";
+fibos.core_symbol = "EOS";
+
+fibos.start();
